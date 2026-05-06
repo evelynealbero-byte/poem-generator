b4b1a80e297288534655315a6d1e819c7c198e98
Evelyne Albero <evelyne.albero@gmail.com>
2026-05-06 12:03:50 +0200

    plugins

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  let poemElement = document.querySelector("#poem");
+function displayPoem(response) {
+  console.log("Poem generated:");
 
   new Typewriter("#poem", {
-    strings: "Roses are red",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "a0bdfcad3964e6e3300b74o509fbat1b";
+  let prompt = `instructions:Generate a spanish poem about ${instructionsInput.value}`;
+  let context =
+    "You are a modern poet expert and like to write shorts and original poems, your goal is to write a 3 line poem inbasic HTML. Make sure to follow the instructions";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem...");
+  console.log(`prompt:${prompt}`);
+  console.log(`context:${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
